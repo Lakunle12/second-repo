511d5899cc80b1ef1ca9167f4670901f9ba0d4fc
Ahmad Olakunle <Ahmadmojeed@gmail.com>
2021-08-24 05:09:31 +0300

    Just testing fr

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-B
-C
-C
-C
+
+#Using the If Statement with Else.
+
+a = "Ahmad is gonna be a Data Scientist"
+b = "Inshallah"
+if b > a:
+  print("Inshallah Ahmad is gonna be a Data Scientist")
+elif a == b:
+  print("No Way")
+else:
+  print("Inshallah Ahmad will get the best")
 
 
+#Defining a function
+def my_function(name, oriki):
+  print(name + " " + oriki)
 
+my_function("Ahmad", "Olakunle") 
 
-C
-print('elh
+#A math ish
+def math(x):
+  return 5 * x
+
+print(math(3))
+print(math(5))
+print(math(9)) 
